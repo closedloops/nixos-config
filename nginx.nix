cd77ad0cb4bf48a1f68d4288c485cf80c0c19914
Christian Wesselhoeft <hi@xtian.us>
2020-05-17 10:13:47 -0600

    nginx.nix: Add more vhost configs

--- nginx.nix
+++ nginx.nix
@@ -4,19 +4,43 @@
   services.nginx = {
     enable = true;
 
+    virtualHosts."alexbaumhoer.com" = {
+      serverAliases = [ "www.alexbaumhoer.com" ];
+      enableACME = true;
+      forceSSL = true;
+      root = "/srv/www/alexbaumhoer.com";
+    };
+
     virtualHosts."dir.xtian.us" = {
       enableACME = true;
       forceSSL = true;
-      root = "/srv/www/dir.xtian.us/public_html";
+      root = "/srv/www/dir.xtian.us";
       extraConfig = ''
         autoindex on;
         autoindex_exact_size off;
       '';
     };
+
+    virtualHosts."katepembrooke.com" = {
+      serverAliases = [ "www.katepembrooke.com" ];
+      enableACME = true;
+      forceSSL = true;
+      root = "/srv/www/katepembrooke.com";
+    };
+
+    virtualHosts."xtian.us" = {
+      serverAliases = [ "www.xtian.us" ];
+      enableACME = true;
+      forceSSL = true;
+      root = "/srv/www/xtian.us";
+    };
   };
 
   security.acme.acceptTerms = true;
   security.acme.certs = {
+    "alexbaumhoer.com".email = "alexbaumhoer@gmail.com";
     "dir.xtian.us".email = "hi@xtian.us";
+    "katepembrooke.com".email = "alexbaumhoer@gmail.com";
+    "xtian.us".email = "hi@xtian.us";
   };
 }
